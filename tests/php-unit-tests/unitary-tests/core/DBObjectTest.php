c8380c086a7b24ec11316d3c46271ca4ecb05e8a
acognet <anne-catherine.cognet@combodo.com>
2023-07-31 15:39:28 +0200

    N°3448 - Framework field size check not correctly implemented for multibytes languages/strings

--- tests/php-unit-tests/unitary-tests/core/DBObjectTest.php
+++ tests/php-unit-tests/unitary-tests/core/DBObjectTest.php
@@ -897,4 +897,61 @@ class DBObjectTest extends ItopDataTestCase
 		return $oPerson;
 	}
 
+
+	/**
+	 * Data provider for test CheckValue
+	 *
+	 * @return array data
+	 */
+	public function getCheckLongValueProvider()
+	{
+		return [
+			'Title with ééé'            => [
+				'title',
+				'ééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééé',
+			],
+			'Title with smiley'         => [
+				'title',
+				'1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111😃😃😃😃😃😃😃😃😃',
+			],
+			'Title with 255 characters' => [
+				'title',
+				'012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234',
+			],
+		];
+	}
+
+	/**
+	 * Test attribute integer incrementation.
+	 *
+	 * @covers       DBObject::DBIncrement
+	 *
+	 * @dataProvider getCheckLongValueProvider
+	 *
+	 */
+	public function testCheckLongValue(string $sAttrCode, string $sValue)
+	{
+		// Create a UserRequest with 2 contacts
+		$oTicket = MetaModel::NewObject('UserRequest', [
+			'ref'         => 'Test Ticket',
+			'title'       => 'Create OK',
+			'description' => 'Create OK',
+			'caller_id'   => 15,
+			'org_id'      => 3,
+		]);
+		/*$oPerson = MetaModel::NewObject('Person');
+
+			$oPerson->Set('name', 'My last name');
+			$oPerson->Set('first_name', 'My first name');
+			$oPerson->Set('org_id', $this->getTestOrgId());*/
+
+		$oTicket->Set($sAttrCode, $sValue);
+		[$bCheckStatus, $aCheckIssues, $bSecurityIssue] = $oTicket->CheckToWrite();
+		$this->assertTrue($bCheckStatus);
+
+		$oTicket->SetTrim($sAttrCode, $sValue);
+		$this->assertEquals($sValue, $oTicket->Get($sAttrCode));
+
+	}
+
 }
